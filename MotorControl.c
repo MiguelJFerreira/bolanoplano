bf651d819b83c9550789f04fb2739d094fa18b75
MiguelJFerreira <migueljferreira@ua.pt>
2017-03-15 21:59:32 +0000

    casos dos pwm_on fora do aceitavel

--- MotorControl.c
+++ MotorControl.c
@@ -8,16 +8,16 @@
 #define PBUSCLK 40000000L // Peripheral bus clock
 
 
-int Move() {
+int Move(int dutycycle) {
   //declaraçao de variaveis
   unsigned int pwm_period=20000; //T do PWM em ms
-  unsigned int pwm_on=1500; //T on do pwm em ms
-
-
+   
   // Performance optimization (flash access time, enable instruct and data cache,... and PBClock setup
+             //Timer 2 is the clock source of OCM
+  
   SYSTEMConfigPerformance(SYSCLK);
   mOSCSetPBDIV( OSC_PB_DIV_2 ); // This is necessary since SYSTEMConfigPerformance defaults FPBDIV to DIV_1
-
+  
   //Set timer
   T2CONbits.ON=0;                 //Stop timer
   IFS0bits.T2IF=0;                //Reset flag interrupt
@@ -28,16 +28,28 @@ int Move() {
 
   //Set OC1
   OC1CONbits.OCM=6;                 //OCM=0b110: OC1in PWM mode
-  OC1CONbits.OCTSEL=0;              //Timer 2 is the clock source of OCM
-  OC1RS=pwm_on/(256/(float)(PBUSCLK/1000000));  //Compute OC1xRS value
+  OC1CONbits.OCTSEL=0;  
   OC1CONbits.ON=1;                  //Enable OC1
-
 
   //Start PWM generation
   T2CONbits.TON=1;                  //Start the timer
 
+  
   while (1) {
-    /* code */
+      
+      ON:
+      unsigned int pwm_on=dutycycle; //T on do pwm em ms, meter aqui a formula
+      
+      if (pwm_on < 750){ 
+          return(EXIT_FAILURE);
+      }
+      else if(pwm_on > 2250){
+          return(EXIT_FAILURE);
+      }
+      else{
+         OC1RS=pwm_on/(256/(float)(PBUSCLK/1000000));  //Compute OC1xRS value 
+      }
+    
   }
 
 return(EXIT_SUCCESS);

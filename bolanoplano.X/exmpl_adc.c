e57eeebb00950d78a0cfe97a353b42a32a631829
MiguelJFerreira <migueljferreira@ua.pt>
2017-03-26 15:32:50 +0100

    Project Moved

---
+++ bolanoplano.X/exmpl_adc.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   exmpl_adc.c
+ * Author: MiguelFerreira
+ *
+ * Created on 7 de Março de 2017, 15:05
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
